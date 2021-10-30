1cee36e3f948ead83efbb18f119118d5faf918e0
phuongle6428 <phuongle6428@gmail.com>
2021-10-30 21:38:34 +0700

    redux setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import HomePage from './modules/homePage/pages/HomePage';
 
 
 function App() {
