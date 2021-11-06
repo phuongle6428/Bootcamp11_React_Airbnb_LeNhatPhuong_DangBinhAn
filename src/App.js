6532124fe2b2ae544c513738bed3496f22143f5d
phuongle6428 <phuongle6428@gmail.com>
2021-11-06 14:50:19 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import Login from './modules/auths/pages/Login';
+import Register from './modules/auths/pages/Register';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -11,16 +13,27 @@ function App() {
   return (
     <div className="app">
       <Router>
-        <Header/>
         <Switch>
-          <Route path="/search">
-            {/* SearchPage */}
+          <Route exact path={["/", "/search"]}>
+            <Header />
+            <Switch>
+              <Route path="/search">
+                {/* SearchPage */}
+              </Route>
+              <Route path="/">
+                <HomePage />
+              </Route>
+            </Switch>
+            <Footer/>
           </Route>
-          <Route path="/">
-            <HomePage/>
+          <Route path="/login">
+            <Login/>
           </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Router path="/profile"></Router>
         </Switch>
-        <Footer/>
       </Router>
     </div>
   );
